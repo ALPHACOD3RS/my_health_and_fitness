0685c63b7f60d423bca66a9e91affb75cc501e82
Naol Ketema <naolketema55@gmail.com>
2023-09-06 13:48:36 +0300

    some colors added

---
+++ lib/src/core/styles/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color backGroundColor = const Color(0xFFF4F6FA);
+Color textColor = const Color(0xFF2D3142);
+Color customSubmitColor = const Color(0xFF7265E3);
